146c28784cf27757b730a57ab4172592b08eb83a
melvincurinier <77935728+melvincurinier@users.noreply.github.com>
2023-04-14 17:15:46 +0200

    express module + server run

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World !");
+const express = require("express");
+
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`);
+});
