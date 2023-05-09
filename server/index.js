d21f4999458e56b30e146890d796e808080a4f19
melvincurinier <77935728+melvincurinier@users.noreply.github.com>
2023-05-09 14:04:14 +0200

    connection with mongodb

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const PORT = 3000;
+
+const db = "mongodb+srv://melvincurinier:Bk1LSaiSnUIGN7YI@bddmdl.cfed4j1.mongodb.net/?retryWrites=true&w=majority"
+
 const app = express();
 
-app.listen(PORT, function () {
+mongoose
+    .connect(db)
+    .then(() => {
+        console.log('Connection Succesful');
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
