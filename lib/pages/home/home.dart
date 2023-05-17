1117e813587283c5e541fa7243f174d9c45cdea9
hugobrisset <hugobrisset@orange.fr>
2023-05-17 20:50:54 +0200

    1

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Home Page"),
-      );
+        child: Container(
+      height: 300,
+      width: 200,
+      color: Colors.pink[100],
+    ));
   }
 }
