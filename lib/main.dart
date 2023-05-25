9ad91376bcd8f3d3e223d653ad8f0a4661488fdf
hugobrisset <hugobrisset@orange.fr>
2023-05-25 21:06:43 +0200

    ca marche

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_grifforhse/pages/home/home.dart';
-import 'package:flutter_app_grifforhse/pages/resultats/resultats.dart';
-import 'package:flutter_app_grifforhse/pages/profile/profile.dart';
-
-void main() {
-  runApp(MyAPP());
-}
-
-class MyAPP extends StatefulWidget {
-  const MyAPP({super.key});
-
-  @override
-  State<MyAPP> createState() => _MyAPPState();
-}
-
-class _MyAPPState extends State<MyAPP> {
-  int _currentIndex = 0;
-
-  setCurrentIndex(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-/*
 import 'package:flutter_app_grifforhse/providers/user_provider.dart';
 import 'package:flutter_app_grifforhse/screens/home_screen.dart';
 import 'package:flutter_app_grifforhse/screens/signup_screen.dart';
@@ -53,32 +30,11 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     authService.getUserData(context);
-    */
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("GrifforHSE"),
-      ),
-      body: [HomePage(), ResultPage(), ProfilePage()][_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setCurrentIndex(index),
-        selectedItemColor: Color.fromARGB(255, 244, 73, 54),
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add_chart_outlined), label: 'Resultat'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle_rounded), label: 'Profile'),
-        ],
-      ),
-    ));
-    /*
       title: 'Flutter Node Auth',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -87,6 +43,5 @@ class _MyAppState extends State<MyApp> {
           ? const SignupScreen()
           : const HomeScreen(),
     );
-    */
   }
 }
