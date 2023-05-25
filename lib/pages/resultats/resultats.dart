d6d560bb56c981c062d512299667bece4a5acb65
hugobrisset <hugobrisset@orange.fr>
2023-05-25 12:18:50 +0200

    bar de score

--- lib/pages/resultats/resultats.dart
+++ lib/pages/resultats/resultats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_app_grifforhse/pages/resultats/user_score.dart';
 
@@ -8,9 +9,35 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final userScore = Provider.of<UserScore>(context);
-    return Center(
-      child: Text("Hygiène: ${userScore.getScore('Hygiène')}\nSécurité: ${userScore.getScore('Sécurité')}\nEnvironnement: ${userScore.getScore('Environnement')}"),
-      
+    final maxScorePerCategory = 10; // replace with your max score per category
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildScoreBar('Hygiène', Colors.blue, userScore.getScore('Hygiène') / maxScorePerCategory),
+            _buildScoreBar('Sécurité', Colors.red, userScore.getScore('Sécurité') / maxScorePerCategory),
+            _buildScoreBar('Environnement', Colors.green, userScore.getScore('Environnement') / maxScorePerCategory),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildScoreBar(String category, Color color, double percent) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: LinearPercentIndicator(
+        lineHeight: 20.0,
+        percent: percent,
+        backgroundColor: Colors.grey,
+        progressColor: color,
+        center: Text(
+          '$category: ${(percent * 100).toStringAsFixed(1)}%',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
